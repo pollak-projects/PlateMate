63870be45aa8de7ab79cdc4de9071ff7ac199ac8
csan <102951810+csanszy@users.noreply.github.com>
2025-02-12 08:33:57 +0100

    orderRoutes

---
+++ server/routes/orderRoutes.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const { roleCheck } = require("../middlewares/roleHandler") 
+var Router = express.Router()
+
+const{
+    getAllOrders,
+    getOrderById,
+    createOrder,
+    deleteOrder,
+    deleteOrderByArray,
+    getAllInProcessOrders,
+    getAllFinishedOrders,
+    getAllServedOrders,
+    setDoneOrder,
+    rollbackDoneOrder,
+    setServedOrder,
+    rollbackServedOrder,
+    getOrdersByTableId
+} = require('../controllers/orderController.js')
+
+Router.get('/', roleCheck(['admin', 'waiter', 'cashier']), getAllOrders);
+
+Router.get('/in-process/', roleCheck(['admin', 'waiter', 'cashier', 'chef']), getAllInProcessOrders);
+
+Router.get('/finished/', roleCheck(['admin', 'waiter', 'cashier', 'chef']), getAllFinishedOrders);
+
+Router.get('/served/', roleCheck(['admin', 'waiter', 'cashier', 'chef']), getAllServedOrders);
+
+Router.get('/for-checkout/:id', roleCheck(['admin', 'cashier']), getOrdersByTableId);
+
+Router.get('/:id', roleCheck(['admin', 'waiter', 'cashier']), getOrderById);
+
+Router.put('/set-done/:id', roleCheck(['admin', 'waiter', 'cashier']), setDoneOrder);
+
+Router.put('/rollback-done/:id', roleCheck(['admin', 'waiter', 'cashier']), rollbackDoneOrder);
+
+Router.put('/set-served/:id', roleCheck(['admin', 'waiter', 'cashier']), setServedOrder);
+
+Router.put('/rollback-served/:id', roleCheck(['admin', 'waiter', 'cashier']), rollbackServedOrder);
+
+Router.post('/', roleCheck(['admin', 'waiter']), createOrder);
+
+Router.delete('/mass-delete/', roleCheck(['admin', 'waiter']), deleteOrderByArray);
+
+Router.delete('/:id', roleCheck(['admin', 'waiter']), deleteOrder);
+
+module.exports = Router
